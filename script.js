dbc938a67698266637549a4962e9a16a425b7ce0
Denys <go4denys@gmail.com>
2023-02-07 16:06:35 -0500

    change 'cocktail' for 'drink' in classes and id

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const searchBtn = document.getElementById('search-btn');
-const cocktailList = document.getElementById('cocktail');
-const cocktailDetailsContent = document.querySelector('.cocktail-details-content');
+const drinkList = document.getElementById('drink');
+const drinkDetailsContent = document.querySelector('.drink-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 //event listeners
-searchBtn.addEventListener('click', getCocktailList);
-cocktailList.addEventListener('click', getCocktailRecipe);
+searchBtn.addEventListener('click', getDrinkList);
+drinkList.addEventListener('click', getDrinkRecipe);
 recipeCloseBtn.addEventListener('click', () => {
-    cocktailDetailsContent.parentElement.classList.remove('showRecipe');
+    drinkDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-//get cocktail list that matches with the ingredients
-function getCocktailList() {
+//get drink list that matches with the ingredients
+function getDrinkList() {
     let searchInputText = document.getElementById('search-input').value.trim();
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
         .then(response => response.json())
@@ -20,27 +20,27 @@ function getCocktailList() {
             if (data.drinks) {
                 data.drinks.forEach(drink => {
                     html += `
-                    <div class="cocktail-item" data-id="${drink.idDrink}">
-                        <div class="cocktail-img">
+                    <div class="drink-item" data-id="${drink.idDrink}">
+                        <div class="drink-img">
                             <img src="${drink.strDrinkThumb}" alt="margarita">
                         </div>
-                        <div class="cocktail-name">
+                        <div class="drink-name">
                             <h3>${drink.strDrink}</h3>
                             <a href="#" class="recipe-btn">Get Recipe</a>
                         </div>
                     </div>`
                 });
-                cocktailList.classList.remove('notFound');
+                drinkList.classList.remove('notFound');
             } else {
                 html = "Sorry, we didn't find any drink!";
-                cocktailList.classList.add('notFound');
+                drinkList.classList.add('notFound');
             }
-            cocktailList.innerHTML = html;
+            drinkList.innerHTML = html;
         });
 }
 
-// get recipe of the cocktail
-function getCocktailRecipe(e) {
+// get recipe of the drink
+function getDrinkRecipe(e) {
     e.preventDefault();
     if (e.target.classList.contains('recipe-btn')) {
         let drinkItem = e.target.parentElement.parentElement;
@@ -57,19 +57,19 @@ function drinkRecipeModal(drink) {
     drink = drink[0];
     let html = `
         <h2 class="recipe-title">${drink.strDrink}</h2>
-        <p class="recipe-category">${drink.strCategory}</p>
+        <p class="recipe-category">Category: ${drink.strCategory}</p>
         <div class="recipe-instruct">
             <h3>Instructions:</h3>
             <p>${drink.strInstructions}</p>
         </div>
-        <div class="recipe-cocktail-img">
+        <div class="recipe-drink-img">
             <img src="${drink.strDrinkThumb}" alt="">
         </div>
         <div class="recipe-link">
             <a href="${drink.strVideo}" target="_blank">Watch Video</a>
         </div>
     `;
-    cocktailDetailsContent.innerHTML = html;
-    cocktailDetailsContent.parentElement.classList.add('showRecipe');
+    drinkDetailsContent.innerHTML = html;
+    drinkDetailsContent.parentElement.classList.add('showRecipe');
     
 }
