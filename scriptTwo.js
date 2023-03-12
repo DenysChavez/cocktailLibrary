7f9b92bc696726290ee5cf2680c79d032130a79a
Denys <go4denys@gmail.com>
2023-03-12 18:14:03 -0400

    display random Recipes with their instructions

--- scriptTwo.js
+++ scriptTwo.js
@@ -2,7 +2,13 @@ const drinkBox = document.getElementById('drink-result');
 const drinkList = document.getElementById('drink');
 const searchBtn = document.getElementById('search-btn');
 const drinkDetailsContent = document.querySelector('.drink-details-content');
-const recipeCloseBtn = document.getElementById('.recipe-close-btn');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// Events Listeners
+drinkList.addEventListener('click', getDrinkRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    drinkDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
 
 displayRandomDrinks()
@@ -31,9 +37,77 @@ function displayRandomDrinks() {
     }
 }
 
+// get recipe of the drink
+function getDrinkRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('recipe-btn')) {
+        let drinkItem = e.target.parentElement.parentElement;
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkItem.dataset.id}`)
+            .then(response => response.json())
+            .then(data => drinkRecipeModal(data.drinks));
+    }
+}
 
+// Create html of the Get Recipe's Display(Modal)
+function drinkRecipeModal(drink) {
+    drink = drink[0];
 
+    // save ingredients in an object
+    let ingredients = Object.fromEntries(Object.entries(drink)
+        .filter(([key, value]) => key.includes("strIngredient")));
+    let measures = Object.fromEntries(Object.entries(drink)
+        .filter(([key, value]) => key.includes("strMeasure")));
+    
+    // cocktail variable holds our merged data witouth null keys and change value of keys where their values are null 
+    let cocktail = {};
+    for (let key in ingredients) {
+        if (ingredients.hasOwnProperty(key) && ingredients[key] !== null) {
+            let ingredient = ingredients[key];
+            let measure = measures["strMeasure" + key.slice(13)];
+            if (measure === null) {
+                measure = "of your preference"
+            }
+            cocktail[ingredient] = measure
+        }
+    }
 
+    // create List of Ingredientes for the Recipe HTML
+    let ingredientsHtml = "";
+    for (let key in cocktail) {
+        ingredientsHtml += `<li>${key}: ${cocktail[key]}</li>`
+    }
+
+    //save Recipe's Instructions
+    let instructions = drink.strInstructions.trim().split(".");
+    instructions = instructions.filter(word => word !== "")
+    let instructionsHtml = ""
+    for (let i = 0; i < instructions.length; i++) {
+        instructionsHtml += `${instructions[i]}<br>`
+    }
+
+      //add everything in the html
+      let html = `
+      <h2 class="recipe-title">${drink.strDrink}</h2>
+      <p class="recipe-category">Category: ${drink.strCategory}</p>
+      <br>
+      <div class="recipe-ingredients">
+          <br>
+          <h3>Ingredients:</h3>
+          <ul>
+              ${ingredientsHtml}
+          </ul>
+      </div>
+      <div class="recipe-instruct">
+          <h3>Instructions:</h3>
+          <p>${instructionsHtml}</p>
+      </div>
+      <div class="recipe-drink-img">
+          <img src="${drink.strDrinkThumb}" alt="">
+      </div>
+  `;
+  drinkDetailsContent.innerHTML = html;
+  drinkDetailsContent.parentElement.classList.add('showRecipe');
+}
 
 
 
